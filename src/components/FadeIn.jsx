8f874d8d2cb7046760056c916d5e48c05099707d
4004ArulmaniV <varulmani13@gmail.com>
2026-07-17 23:28:21 +0530

    Test disable FadeIn animation

--- src/components/FadeIn.jsx
+++ src/components/FadeIn.jsx
@@ -1,41 +1,9 @@
-import { motion } from "framer-motion";
-
-
-const FadeIn = ({ children, delay = 0 }) => {
-
+function FadeIn({children}) {
   return (
-
-    <motion.div
-
-      initial={{
-        opacity: 0,
-        y: 30,
-      }}
-
-      whileInView={{
-        opacity: 1,
-        y: 0,
-      }}
-
-      transition={{
-        duration: 0.6,
-        delay,
-      }}
-
-      viewport={{
-        once: true,
-        amount: 0.5,
-      }}
-
-    >
-
+    <>
       {children}
-
-    </motion.div>
-
+    </>
   );
-
-};
-
+}
 
 export default FadeIn;
